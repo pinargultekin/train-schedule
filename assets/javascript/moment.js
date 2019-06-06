71854bb91bf85ba872e8ce0ba8fcb7c57e0a5748
29bucuk <pinargultekin@yahoo.com>
2019-06-06 09:50:31 -0400

    html page is designed using css & database configuration is created

---
+++ assets/javascript/moment.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyB-f2qsrgTHWgO2uj2wOY9L9vJSWFau74Q",
+        authDomain: "pinar-project-d0461.firebaseapp.com",
+        databaseURL: "https://pinar-project-d0461.firebaseio.com/",
+        projectId: "pinar-project-d0461",
+        storageBucket: "pinar-project-d0461.appspot.com",
+        messagingSenderId: "228878479960",
+        appId: "1:228878479960:web:e27af08b50e4245e"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //reference the database
+    var database = firebase.database();
+
+
+});
