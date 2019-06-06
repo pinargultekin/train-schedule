4af595f5eca2e3438b4735ea485b0b31f7e0432f
29bucuk <pinargultekin@yahoo.com>
2019-06-06 09:52:24 -0400

    click function added for submit button

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -15,5 +15,56 @@ $(document).ready(function () {
     //reference the database
     var database = firebase.database();
 
+    // Submit button click function
+    $("#submit-input").on("click", function (event) {
+        event.preventDefault();
+        var trainName = $("#train-name").val().trim();
+        var destination = $("#destination").val().trim();
+        var trainTime = $("#train-time").val().trim();
+        var frequency = parseInt($("#frequency").val().trim());
+        console.log(trainName);
+        console.log(destination);
+        console.log(trainTime);
+        console.log(frequency);
+        var train = {
+            trainName: trainName,
+            destination: destination,
+            trainTime: trainTime,
+            frequency: frequency,
+        }; console.log(train);
 
+     
+
+    });
+
+
+
+    //     //update the page in real-time function
+    //     database.ref().on("value", function (snapshot) {
+    //         if (snapshot.child("trainName").exists() && snapshot.child("destination").exists()
+    //             && snapshoot.child("trainTime").exists() && snapshot.child("frequency").exists()) {
+    //         }
+
+    //         console.log(trainName);
+    //         console.log(destination);
+    //         console.log(trainTime);
+    //         console.log(frequency);
+    //         $("#train-name").text(trainName);
+    //         $("#destination").text(destination);
+    //         $("#train-time").text(trainTime);
+    //         $("#frequency").text(frequency);
+    //     }, function (errorObject) {
+    //         console.log("The read failed: " + errorObject.code);
+    //     });
+
+
+    //     //current date & time
+    //     function currentDate() {
+    //         var today = new Date();
+    //         var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    //         var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    //         var dateTime = date + ' ' + time;
+    //         console.log(dateTime);
+    //     };
+    //     currentDate();
 });
